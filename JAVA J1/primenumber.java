6c8567b6d9e4921ca12aee03b13a0d92b2bcf65d
Aditya Shenvi <95111404+aditya4232@users.noreply.github.com>
2025-09-17 18:09:39 +0530

    sep17-1

---
+++ JAVA J1/primenumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class primenumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= n-1; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("It's a prime number");
+        } else {
+            System.out.println("Not a prime number");
+        }
+    }
+}
