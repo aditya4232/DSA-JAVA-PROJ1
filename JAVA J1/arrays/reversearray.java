7cd88f28f808eac48244f1a261c64051980dc675
Aditya Shenvi <adityashenvi2050@gmail.com>
2025-10-06 16:46:10 +0530

    Update reversearray.java

--- JAVA J1/arrays/reversearray.java
+++ JAVA J1/arrays/reversearray.java
@@ -20,7 +20,7 @@ public class reversearray {
 
     public static void main(String[] args) {
 
-        int numbers[]={2,4,6,8,10}
+        int numbers[]={2,4,6,8,10};
         reverse(numbers);
         for(int i =0;i<numbers.length;i++){
             System.out.print(numbers[i]+ " ");
