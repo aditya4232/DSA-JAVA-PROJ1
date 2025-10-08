082e831e73bf867d74f5e186e0d87634d2707a0a
Aditya Shenvi <adityashenvi2050@gmail.com>
2025-10-08 23:13:26 +0530

    8oct-2Q

---
+++ JAVA J1/arrays/spiralmatrix.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class spiralmatrix {
+
+    public static void printspiral ( int matrix[][]){
+        int startrow = 0;
+        int startcol = 0;
+        int endrow = matrix.length-1;
+        int endcol = matrix[0].length-1;
+
+    } 
+    public static void main(String[] args) {
+        int matrix [][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        
+    }
+    
+}
