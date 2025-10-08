c1de95f3534bf38cc0883937c2411d609c891624
Aditya Shenvi <adityashenvi2050@gmail.com>
2025-10-08 23:43:10 +0530

    Update spiralmatrix.java

--- JAVA J1/arrays/spiralmatrix.java
+++ JAVA J1/arrays/spiralmatrix.java
@@ -8,9 +8,50 @@ public class spiralmatrix {
         int endrow = matrix.length-1;
         int endcol = matrix[0].length-1;
 
+        while(startrow <= endrow && startcol <= endcol){
+
+            //top
+            for(int j = startcol ;j <=endcol ; j++ ){
+                System.out.print(matrix[startrow][j]+ " ");
+            }
+
+            //right
+
+            for(int i = startrow+1; i<= endrow ; i++){
+                System.out.print(matrix[i][endcol] + " ");
+            }
+
+            //bottom 
+
+            for ( int j = endcol-1; j<=startrow; j++){
+                if( startrow == endrow ){
+                    break;
+                }
+                
+                System.out.print(matrix[endrow][j]+ " ");
+            }
+
+            //left 
+            for( int i = endrow-1 ; i<=startrow+1; i++){
+                if( startcol == endcol ){
+                    break;
+                }
+                System.out.println(matrix[i][startcol] + " ");
+            }
+
+            startcol++;
+            startrow++;
+            endcol++;
+            endrow++;
+
+        }
+
+
+
     } 
     public static void main(String[] args) {
         int matrix [][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        printspiral(matrix);
         
     }
     
