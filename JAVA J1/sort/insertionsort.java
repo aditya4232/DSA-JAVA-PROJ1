adf1f9e1c76d584314b278077de9919d95357ef2
Aditya Shenvi <adityashenvi2050@gmail.com>
2025-10-09 13:20:59 +0530

    Create insertionsort.java

---
+++ JAVA J1/sort/insertionsort.java
@@ -0,0 +1,21 @@
+package sort;
+
+public class insertionsort {
+
+    public static void insertionsort(int arr[]){
+        
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i=0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {64, 25, 12, 22, 11};
+                System.out.println("Sorted array");
+        printArray(arr);
+    }
+    
+}
