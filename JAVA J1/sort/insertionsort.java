a0f238635507f93971c411139a98eae217c14a6b
Aditya Shenvi <adityashenvi2050@gmail.com>
2025-10-09 13:32:52 +0530

    Update insertionsort.java

--- JAVA J1/sort/insertionsort.java
+++ JAVA J1/sort/insertionsort.java
@@ -3,18 +3,32 @@ package sort;
 public class insertionsort {
 
     public static void insertionsort(int arr[]){
-        
+        for(int i =1; i<arr.length;i++){
+        int curr = arr[i];
+        int prev = i-1;
+
+        while(prev >=0  && arr[prev] > curr ){
+            arr[prev+1]= arr[prev];
+            prev--;
+
+        }
+
+        arr[prev+1] = curr;
+        }
+
+
     }
 
     public static void printArray(int arr[]) {
-        for (int i=0; i < arr.length; ++i)
+        for (int i=0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
 
     public static void main(String args[]) {
         int arr[] = {64, 25, 12, 22, 11};
-                System.out.println("Sorted array");
+        insertionsort(arr);
+        System.out.println("Sorted array");
         printArray(arr);
     }
     
