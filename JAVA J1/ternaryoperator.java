3b27918988c0f414e8d300056c834db48bee0ae1
Aditya Shenvi <adityashenvi2050@gmail.com>
2026-04-19 16:51:30 +0530

    d1

--- JAVA J1/ternaryoperator.java
+++
@@ -1,7 +0,0 @@
-public class ternaryoperator {
-    public static void main(String[] args) {
-        int number = 5;
-        String type = ((number%2) == 0) ? "even" : "odd";
-        System.err.println(type);
-    }
-}
--- JAVA J1/typecasting.java
+++
@@ -1,9 +0,0 @@
-public class typecasting {
-
-    public static void main(String[] args) {
-        
-
-        
-    }
-    
-}
